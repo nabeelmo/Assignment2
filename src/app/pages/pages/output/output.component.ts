91b762bcdd33cc7f3d5fe2616268d7fdfd935265
Nabeel <nabeelmo@shernet.sheridancollege.ca>
2025-02-18 11:22:32 -0500

    Create the input an output pages

---
+++ src/app/pages/pages/output/output.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-output',
+  standalone: false,
+  templateUrl: './output.component.html',
+  styleUrl: './output.component.css'
+})
+export class OutputComponent {
+
+}
